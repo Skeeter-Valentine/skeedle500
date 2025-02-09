fea8e39bc95f600741a8bc7b6307d3c5af2e008d
unknown <brendencyr24@gmail.com>
2025-02-08 18:12:37 -0700

    error at word comparison

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import { createContext, useEffect, useState } from 'react'; 
+import { boardDefault, generateWordSet } from './Words'
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0})
+  const [wordSet, setWordSet] = useState(new Set())
+
+  const correctWord = "RIGHT";
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet)
+      console.log(words)
+    })
+  }, [])
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return; 
+    const newBoard = [...board]
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1})
+  }
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board]
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1})
+  }
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+
+    console.log((currWord.toLowerCase() + " "))
+    console.log(wordSet.has(currWord.toLowerCase() + " "))
+
+    if (wordSet.has(currWord.toLowerCase() + " ")) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
+    } else {
+      alert("Word Not Found")
+    }
+
+    setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav>
+      <h1>Skeedle500</h1>
+     </nav>
+     <AppContext.Provider value={{
+        board, 
+        setBoard, 
+        currAttempt, 
+        setCurrAttempt, 
+        onDelete, 
+        onEnter, 
+        onSelectLetter,
+        correctWord
+      }}>
+      <div className='game'>
+        <Board />
+        <Keyboard />
+      </div>
+     </AppContext.Provider>
     </div>
   );
 }
